b646aa1c1de26576c7f2704946124eb383bea67b
Cody Leedom <Cody@uruktek.com>
2020-06-25 18:38:49 -0500

    progress bar is working from test code give by dev. Issues seems that all params need to be filled for it to be used correctly.

--- src/fasting.js
+++ src/fasting.js
@@ -1,11 +1,29 @@
-var Progressbar = require('progressbar.js');
-
-var bar = new Progressbar.SemiCircle(container, {
+var bar = new ProgressBar.SemiCircle(container, {
     strokeWidth: 6,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
     easing: 'easeInOut',
-    duration: 1400,
-    color: '#ffffffff',
-    svgStyle: null
-});
+    duration: 700,
+    svgStyle: null,
+    from: {color: '#000000'},//'#FFEA82'
+    to: {color: '#ED6A5A'},
+    // Set default step function for all animate calls
+    step: (state, bar) => {
+      bar.path.setAttribute('stroke', state.color);
+      var value = Math.round(bar.value() * 100);
+      if (value === 0) {
+        bar.setText('');
+      } else {
+        bar.setText(value);
+      }
+  
+      bar.text.style.color = state.color;
+    }
+  });
+  bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+  bar.text.style.fontSize = '1.5rem';
+  
+  bar.animate(1);  // Number from 0.0 to 1.0
 
-bar.animate(1.0);
+  //example pulled from progressbar js
